807bbfc4b11389a9ee3206c0fc254a9a70584a65
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-09-20 10:02:13 -0300

    Sem16_T1_Q3

---
+++ Sem16_T1/Q3_soma_media_maior_menor.py
@@ -0,0 +1,71 @@
+# Fazer um programa para ler uma matriz n x m de números inteiros. Os valores de n e m são inteiros, positivos e  devem ser informados pelo usuário, calcular e armazenar em uma tupla para mostrar, respectivamente: 
+
+def vetor(n):
+    vetor = []
+    for i in range(n):
+        valor = int(input('Valor: '))
+        vetor.append(valor)
+    return vetor
+
+def matriz(n,m):
+    matriz = []
+    for i in range (m):
+        linha = vetor(n)
+        matriz.append(linha)
+    return matriz
+
+# a) a soma dos elementos da primeira linha 
+def soma_lin_1 (m, ler_matriz):
+    soma = 0
+    # ler_matriz = ler_matriz
+    for i in range (m):
+        valor = ler_matriz[0][i]
+        soma += valor
+    return soma
+
+# b) a soma dos elementos da última coluna 
+def soma_ultima_col(n, ler_matriz):
+    soma = 0
+    # ler_matriz = matriz(n,m,)
+    for i in range (n):
+        valor = ler_matriz[i][-1]
+        soma += valor
+    return soma
+
+# c) a média de todos os elementos 
+def media(n,m, ler_matriz):
+    soma = 0
+    # ler_matriz = matriz(n,m, ler_matriz)
+    for n in range (m):
+        for m in range (n):
+            valor = ler_matriz[n][m]
+            soma += valor
+    media = soma/(n*m)
+    return media
+
+# d) o menor elemento 
+def menor(n,m, ler_matriz):
+    menor = ler_matriz[0] [0]
+    for n in range(m):
+        for m in range(n):
+            if ler_matriz[n][m] < menor:
+                menor = ler_matriz[n][m]
+    return menor
+
+# e) o maior elemento 
+def maior(n,m, ler_matriz):
+    maior = ler_matriz[0] [0]
+    for n in range(m):
+        for m in range(n):
+            if ler_matriz[n][m] > maior:
+                maior = ler_matriz[n][m]
+    return maior
+
+def main():
+    n = int(input('Número de colunas: '))
+    m = int(input('Número de linhas: '))
+    ler_matriz = matriz(n,m)
+    tupla = (soma_lin_1(n, ler_matriz), soma_ultima_col(n, ler_matriz), media(n,m, ler_matriz), menor(n,m, ler_matriz), maior(n,m, ler_matriz))
+    print (tupla)
+if __name__=='__main__':
+    main()
